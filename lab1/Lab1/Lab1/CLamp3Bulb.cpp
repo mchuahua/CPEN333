5c50057ba0653f2b81c28650f475d33891f3ce3d
mchuahua <43918886+mchuahua@users.noreply.github.com>
2020-09-12 18:48:57 -0700

    Exercise 3 + 4 done

--- lab1/Lab1/Lab1/CLamp3Bulb.cpp
+++ lab1/Lab1/Lab1/CLamp3Bulb.cpp
@@ -1,15 +1,113 @@
 #include "CLamp3Bulb.h"
 #include <iostream>
 
-//Default Constructor with three inputs
+//Default Constructor with three inputs. Constructs lamp and sets wattage of three bulbs, with state off
 CLamp3Bulb::CLamp3Bulb(int w1, int w2, int w3){
-	state = 0;
-	watts = 0;
-	std::cout << "CBulb default Constructor is being called.." << std::endl;
-}
+	
+    // Exercise 3
+    // bulb_arr[0].setwatts(w1);
+	// bulb_arr[1].setwatts(w2);
+	// bulb_arr[2].setwatts(w3);
+    
+    // Exercise 4
+    bulb_arr[0] = new CBulb(w1);
+	bulb_arr[1] = new CBulb(w2);
+	bulb_arr[2] = new CBulb(w3);
+    my_switch = new CSwitch();
 
+	std::cout << "CLamp3Bulb default Constructor is being called.." << std::endl;
+}
 
 //Deconstructor
 CLamp3Bulb::~CLamp3Bulb(){
-	std::cout <<"CLamp3Bulb desctructor being called.." << std::endl;
+    // Exercise 4
+    delete bulb_arr[0];
+    delete bulb_arr[1];
+    delete bulb_arr[2];
+    delete my_switch;
+	std::cout << "CLamp3Bulb destructor being called.." << std::endl;
+}
+
+//Turn switch and all three bulbs on
+void CLamp3Bulb::LampOn(){
+    // Turn switch on
+    // Exercise 3
+    // my_switch.turnon();
+    // Exercise 4
+    my_switch->turnon();
+    
+    //Turn all bulbs on
+    for (int i = 0; i < 3; ++i){
+        // Exercise 3
+        // bulb_arr[i].on();
+        // Exercise 4
+        bulb_arr[i]->on();
+    }
+}
+
+// Turn switch and 3 bulbs off 
+void CLamp3Bulb::LampOff(){
+    //Turn switch off 
+    // Exercise 3
+    // my_switch.turnoff();
+    // Exercise 4
+    my_switch->turnoff();
+    //Turn all bulbs off 
+    for (int i = 0; i < 3; ++i){
+        // Exercise 3
+        // bulb_arr[i].off();
+        // Exercise 4
+        bulb_arr[i]->off();
+    }
+}
+
+// print state of lamp, ie on or off 
+void CLamp3Bulb::print(){
+    // Exercise 3
+    // my_switch.print();
+    // Exercise 4
+    my_switch->print();
+
+}
+
+// returns state of switch in lamp
+int CLamp3Bulb::getState(){
+    // Exercise 3
+    // return my_switch.getState();
+    // Exercise 4
+    return my_switch->getState();
+}
+
+// return power currently actually being consumed by lamp based on state 
+int CLamp3Bulb::getPower(){
+    int total_power = 0;
+    //Adds all power used by bulbs if switch is on
+    // Exercise 3
+    // if (my_switch.getState() != 0){
+    //     for (int i = 0; i < 3; ++i){
+    //         total_power += bulb_arr[i].getPower();
+    //     }
+    // Exercise 4
+    if (my_switch->getState() != 0){
+        for (int i = 0; i < 3; ++i){
+            total_power += bulb_arr[i]->getPower();
+        }
+        
+    }
+     
+    return total_power;
+}
+
+CBulb* CLamp3Bulb::exchange(CBulb* newBulb, int bulbNum){
+    if (bulbNum < 0 || bulbNum > 2){
+        std::cout << "ERROR, bulb number is not in range!" << std::endl;
+        return NULL;
+    }
+    
+    //Temporary old bulb to return
+    CBulb* oldBulb = bulb_arr[bulbNum];
+
+    bulb_arr[bulbNum] = newBulb;
+
+    return oldBulb;
 }
--- lab1/Lab1/Lab1/CLamp3Bulb.h
+++ lab1/Lab1/Lab1/CLamp3Bulb.h
@@ -14,10 +14,27 @@ public:
 	CLamp3Bulb(int w1, int w2, int w3);
 	//Deconstructor
 	~CLamp3Bulb();
-	
+	//Turn switch and all three bulbs on
+    void LampOn();
+    // Turn switch and 3 bulbs off 
+    void LampOff();
+    // print state of lamp, ie on or off 
+    void print();
+    // returns state of switch in lamp
+    int getState();
+    // return power currently actually being consumed by lamp based on state 
+    int getPower();
+    // Bulb exchange
+    CBulb* exchange(CBulb* newBulb, int bulbNum);
+
 private:
-	CBulb* bulb_arr[3];
-    CSwitch my_switch;
+	//Exercise 3
+    // CBulb bulb_arr[3];
+    // CSwitch my_switch;
+
+    //Exercise 4
+    CBulb* bulb_arr[3];
+    CSwitch* my_switch;
 };
 
 #endif
