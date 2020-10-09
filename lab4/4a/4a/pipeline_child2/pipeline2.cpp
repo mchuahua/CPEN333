821a1f7527f1d59ffa276ff2d15f92c8d344e6cc
mchuahua <43918886+mchuahua@users.noreply.github.com>
2020-10-09 00:09:39 -0700

    lab 4a done

---
+++ lab4/4a/4a/pipeline_child2/pipeline2.cpp
@@ -0,0 +1,48 @@
+//	A Win32 C program to demonstrate the creation and use of name pipe. Error checking has 
+//	been ommitted to aid clarity
+
+#include 	"..\..\..\..\RTExamples\rt.h"		// change pathlist to "rt.h" file to suit location on disk
+
+struct		example {		// structure template for some data that we intend to read from pipeline
+	int 	x;
+	float 	y;
+};
+
+// Some variables to hold the read from the pipeline.
+
+int 	i;				// a simple int
+int 	array1[10]; 		// array of 10 integers
+
+char	name[15];			// a string of 15 characters
+struct	 example mystruct;	// a structure with an int and a float in it
+
+int	main()
+{
+	int 	x;					// a temporary variable for use with the 'for' loop below
+
+	printf("Child Process Opening Pipeline.....\n");
+	CPipe	p1("MyPipe2", 1024);		// Create a pipe 'p1' with the name "MyPipe"
+
+
+	p1.Read(&i, sizeof(i));				// Read the int 'i' from the pipe
+	cout << "Child Process read integer = " << i << " from Pipeline.....\n";
+
+	p1.Read(&array1[0], sizeof(array1));		// Read the array of integers' from the pipe
+
+	printf("Child Process read array = ");
+	for (x = 0; x < sizeof(array1) / sizeof(array1[0]); x++)
+		cout << " " << array1[x];
+
+	putchar('\n');
+
+	p1.Read(&name[0], sizeof(name));	// Read the string from the pipe
+	cout << "Child Process read string = " << name << ", from Pipeline.....\n";
+
+	p1.Read(&mystruct, sizeof(mystruct));	// Read the structure from the pipeline
+	cout << "Child Process read [" << mystruct.x << "," << mystruct.y << "] from Pipeline.....\n";
+
+	Sleep(4000);
+	return 0;
+
+}	// pipe destructor called here to destroy p. Note however if pipeline still has unread data in it,
+	// that data will be preserved
